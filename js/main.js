06f17e86c5300371bc7c0d79cddf49ddfd3bd334
jobsonpvarghese <jobsonvarghese1@gmail.com>
2022-10-25 15:16:49 -0400

    .

--- js/main.js
+++ js/main.js
@@ -9,17 +9,35 @@ document.getElementById("add-song").addEventListener("click", () => {
     title: title.value,
     artist: artist.value
   }
+  validation(song)
+})
 
-  if (song.title && song.artist) {
+// Register the service worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./service-worker.js", { scope: "/pwa-lab2/" })
+      .then(registration => {
+        console.log("Service Worker registered: ", registration)
+      })
+      .catch(registrationError => {
+        console.log("Service Worker registration failed: ", registrationError)
+      })
+  })
+}
+
+// Validation function
+const validation = data => {
+  if (data.title && data.artist) {
     // Add song to the list
     const songContainer = document.getElementsByClassName("song-container") // Get the song container
     const songList = document.createElement("DIV") // Create a div element
     songList.classList.add("song-list") // Add the class song-list to the div element
     songContainer[0].appendChild(songList) // Append the div element to the song container
     songList.innerHTML = `
-      <h3>${song.title}</h3>
-      <h4>${song.artist}</h4>
-  `
+          <h3>${data.title}</h3>
+          <h4>${data.artist}</h4>
+      `
     // Clear the input fields
     title.value = ""
     artist.value = ""
@@ -36,27 +54,27 @@ document.getElementById("add-song").addEventListener("click", () => {
     document.getElementsByClassName("alert-content")[0].innerHTML = "Please fill all the fields"
 
     // Making the input fields red
-    if (song.title == "" && song.artist == "") {
+    if (data.title == "" && data.artist == "") {
       title.style.border = "1px solid red"
       artist.style.border = "1px solid red"
     }
 
     // Making the input fields red if the title is empty
-    if (!song.title) {
+    if (!data.title) {
       title.style.border = "1px solid red"
     } else {
       title.style.border = "1px solid green"
     }
 
     // Making the input fields red if the artist is empty
-    if (!song.artist) {
+    if (!data.artist) {
       artist.style.border = "1px solid red"
     } else {
       artist.style.border = "1px solid green"
     }
 
     // Making the alert disappear after 4 seconds
-    if (song.title && song.artist) {
+    if (data.title && data.artist) {
       setTimeout(() => {
         alert[0].style.display = "none"
         title.style.border = "1px solid grey"
@@ -64,18 +82,4 @@ document.getElementById("add-song").addEventListener("click", () => {
       }, 4000)
     }
   }
-})
-
-// Register the service worker
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("./service-worker.js", { scope: "/pwa-lab2/" })
-      .then(registration => {
-        console.log("Service Worker registered: ", registration)
-      })
-      .catch(registrationError => {
-        console.log("Service Worker registration failed: ", registrationError)
-      })
-  })
 }
