5bc6f790aaf7b560ce6879e0498324257ed3b5cc
jobsonpvarghese <jobsonvarghese1@gmail.com>
2022-10-25 14:51:54 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,81 @@
+var title = document.getElementById("title") // Get the title element
+var artist = document.getElementById("artist") // Get the artist element
+const alert = document.getElementsByClassName("alert") // Get the alert element
+
+// button on click event
+document.getElementById("add-song").addEventListener("click", () => {
+  // Get the value of the input fields
+  var song = {
+    title: title.value,
+    artist: artist.value
+  }
+
+  if (song.title && song.artist) {
+    // Add song to the list
+    const songContainer = document.getElementsByClassName("song-container") // Get the song container
+    const songList = document.createElement("DIV") // Create a div element
+    songList.classList.add("song-list") // Add the class song-list to the div element
+    songContainer[0].appendChild(songList) // Append the div element to the song container
+    songList.innerHTML = `
+      <h3>${song.title}</h3>
+      <h4>${song.artist}</h4>
+  `
+    // Clear the input fields
+    title.value = ""
+    artist.value = ""
+
+    // Making the alert disappear
+    title.style.border = "1px solid grey"
+    artist.style.border = "1px solid grey"
+
+    //Hiding the alert
+    alert[0].style.display = "none"
+  } else {
+    // Making the alert appear
+    alert[0].style.display = "block"
+    document.getElementsByClassName("alert-content")[0].innerHTML = "Please fill all the fields"
+
+    // Making the input fields red
+    if (song.title == "" && song.artist == "") {
+      title.style.border = "1px solid red"
+      artist.style.border = "1px solid red"
+    }
+
+    // Making the input fields red if the title is empty
+    if (!song.title) {
+      title.style.border = "1px solid red"
+    } else {
+      title.style.border = "1px solid green"
+    }
+
+    // Making the input fields red if the artist is empty
+    if (!song.artist) {
+      artist.style.border = "1px solid red"
+    } else {
+      artist.style.border = "1px solid green"
+    }
+
+    // Making the alert disappear after 4 seconds
+    if (song.title && song.artist) {
+      setTimeout(() => {
+        alert[0].style.display = "none"
+        title.style.border = "1px solid grey"
+        artist.style.border = "1px solid grey"
+      }, 4000)
+    }
+  }
+})
+
+// Register the service worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./service-worker.js", { scope: "/pwa-lab2/" })
+      .then(registration => {
+        console.log("Service Worker registered: ", registration)
+      })
+      .catch(registrationError => {
+        console.log("Service Worker registration failed: ", registrationError)
+      })
+  })
+}
---
+++ service-worker.js
@@ -0,0 +1,50 @@
+// cache name
+const cacheName = "cacheAssets-v-alpha-1.0"
+
+// network with cache fallback
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request)
+    })
+  )
+})
+
+// cache assets
+const baseURL = "https://jobsonpvarghese.github.io/pwa-lab2/"
+
+const cacheAssets = [
+  baseURL,
+  `${baseURL}index.html`,
+  `${baseURL}main.js`,
+  `${baseURL}manifest.json`,
+  `${baseURL}service-worker.js`,
+  `${baseURL}style.css`,
+  `${baseURL}assets/musical-note.png`,
+  `${baseURL}assets/musical-note(192x192).png`
+] // Add all the files that you want to cache
+
+// install event
+self.addEventListener("install", function (event) {
+  self.skipWaiting()
+  caches
+    .open(cacheName)
+    .then(function (cache) {
+      console.log("[Service Worker] Installing...")
+      return cache.addAll(cacheAssets)
+    })
+    .catch(err => {
+      console.log("something went wrong", err)
+    })
+})
+
+// activate event
+self.addEventListener("activate", function (event) {
+  console.log("[Service worker] activated", event)
+  event.waitUntil(clients.claim())
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(cacheNames.filter(cache => cache !== cacheName).map(cacheName => caches.delete(cacheName)))
+    })
+  )
+})
